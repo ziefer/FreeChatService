ecd2a21eee3a7d0f72d4621db29e5e2430dac00e
lisyoen <lisyoen@gmail.com>
2013-04-26 01:43:10 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,7 +2,8 @@
 
 var app = require('http').createServer(handler)
     , io = require('socket.io').listen(app)
-    , fs = require('fs');
+    , fs = require('fs')
+    , exec = require('child_process').exec;
 
 app.listen(8003);
 
@@ -22,12 +23,30 @@ function handler (req, res) {
 io.sockets.on('connection', function (socket) {
 	socket.on('my other event', function (data) {
 		console.log(data);
-		socket.emit("my_message", data);
+		socket.emit('my_message', data);
 		socket.broadcast.emit("message", data);
-    });
+	});
 	
 	socket.on('report', function (data) {
 		console.log('request report');
 		socket.emit('my_report', {users: 10, rooms: 3});
 	});
- });
+	
+	function echo_call(cmd, callback) {
+		exec(cmd, function(err, stdout, stderr) {
+			if (err) {
+				console.log(stderr + '\nError:' + err.code, err.code);
+			} else {
+				console.log(stdout);
+			}
+			socket.emit('system report', {err: err, stdout: stdout, stderr: stderr});
+			
+			callback(arguments);
+		});
+	}
+	
+	socket.on('git pull', function (data) {
+		console.log('git pull');
+		echo_call('git pull', function() {});
+	});
+});
